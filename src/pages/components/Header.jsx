4b13a453648f4a46a69806578af69dedbc1805f4
mlmcdan1 <77369368+mlmcdan1@users.noreply.github.com>
2023-08-08 22:49:45 -0500

    create sign in, sign up and forgot password pages and oauth component

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -7,7 +7,6 @@ export default function Header() {
 
     //fast routing from react
     const navigate = useNavigate()
-    console.log(location)
 
     function pathMathRoute(route){
         if(route === location.pathname){
