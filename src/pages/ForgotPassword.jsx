ce80c093c7220028aaa6f8bd61e72696e9740eb2
mlmcdan1 <77369368+mlmcdan1@users.noreply.github.com>
2023-10-31 14:19:25 -0500

    complete forgot password functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import OAuth from './components/OAuth';
-
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import {toast} from 'react-toastify'
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
   
 
   function onChange(e){
     setEmail(e.target.value)
+  }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
   }
   return (
     <section>
@@ -20,8 +32,8 @@ export default function ForgotPassword() {
           className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
-            <input type="email" id='email' value={email} onChange={onChange} placeholder='Email' className='mb-6 w-full px-4 py-2 text-xl
+          <form onSubmit={onSubmit}>
+            <input type="email" id='email' value={email} onChange={onChange} placeholder='Email Address' className='mb-6 w-full px-4 py-2 text-xl
             text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'/>
             <div className='relative mb-6'>
              
@@ -37,7 +49,7 @@ export default function ForgotPassword() {
                     </Link>
               </p>
               <p>
-                <Link to="/forgot-password" className='text-blue-600
+                <Link to="/sign-in" className='text-blue-600
                  hover:text-blue-800
                  transition duration-200
                   ease-in-out'>
