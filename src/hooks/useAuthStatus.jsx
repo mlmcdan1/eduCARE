ea2bdebda192efda98ae684ca8c0e9c10035c796
mlmcdan1 <77369368+mlmcdan1@users.noreply.github.com>
2023-11-04 18:29:22 -0500

    create a private route and custom hook

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,22 @@
+import { onAuthStateChanged,getAuth } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+import React from 'react'
+
+export function useAuthStatus() {
+    const [loggedIn, setLoggedin] = useState(false)
+    const [checkingStatus, setCheckingStatus] = useState(true)
+
+    useEffect(()=>{
+        const auth = getAuth()
+        onAuthStateChanged(auth, (user)=>{
+            //if person is authenticated
+            if(user){
+                setLoggedin(true)
+            }
+            setCheckingStatus(false);
+        })
+    }, [])
+  return (
+    {loggedIn, checkingStatus}
+  )
+}
